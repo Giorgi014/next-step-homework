57e163535d5c57128c9c0c2fdc1a3e292a907398
Giorgi014 <giorgi.gugunava99@gmail.com>
2024-07-29 21:27:45 +0400

    test part

---
+++ app.js
@@ -0,0 +1,12 @@
+const onclick = document.getElementsByClassName('fa-bars')
+
+
+function showSidebar(){
+    const sidebar = document.querySelector('button')
+
+    
+    sidebar.addEventListener("click", () => deleteElement(index))
+    console.log('showSidebar');
+}
+
+showSidebar()
